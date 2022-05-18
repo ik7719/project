705686a7b1e81b3c892afc4dbb8e7020ace73a5b
ik7719 <imlogic@naver.com>
2022-05-19 00:11:09 +0900

    중간 저장

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bus {
     int passengerMax;
     int passengerCurrent;
@@ -15,26 +17,43 @@ public class Bus {
         oil = 0;
         speed = 0;
         condition = "운행";
+
     }
 
-    public void drive(BusCondition Driving, BusCondition Stop){
-        if(condition == BusCondition.Driving.condition){
+    public void stop(){
 
+    }
+
+    public void drive(){
+        if(condition == BusCondition.Stop.condition){
+            System.out.println("차고지행 상태입니다.");
+            return;
         }
-
-        System.out.println(BusCondition.Driving.toString());
 
     }
 
     public void conditionChange(){
+        if (oil < 10) {
+            System.out.println("주유가 필요합니다");
+            return ;
+        }
 
     }
 
     public void ride(){
-
+        if (passengerCurrent > passengerMax){
+            return;
+        }
+        passengerCurrent++;
     }
 
     public void speedChange(){
-
+        if (oil < 10){
+            System.out.print("주유량을 확인해 주세요");
+            return;
+        }
+        Scanner scan = new Scanner(System.in);
+        int plusSpeed = scan.nextInt();
+        speed += plusSpeed;
     }
 }
