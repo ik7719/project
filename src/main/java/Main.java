bfa247dd17ad71a0a5d91c610fef3cf8efbcd51e
ik7719 <imlogic@naver.com>
2022-05-19 20:24:14 +0900

    spring과제 - 버스

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,14 @@
-//import java.util.UUID;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//
-//        Bus bus1 = new Bus(11, 0, 10, 7);
-//
-//        bus1.drive();
-//        bus1.conditionChange();
-//        bus1.ride();
-//        bus1.speedChange();
-//
-//    }
-//}
+import java.util.UUID;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Bus bus1 = new Bus(11, 0, 10, 7);
+
+        bus1.drive();
+        bus1.conditionChange();
+        bus1.ride();
+        bus1.speedChange();
+
+    }
+}
